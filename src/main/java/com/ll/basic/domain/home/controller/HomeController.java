c2557df26ae661b3f57943c3d63d09f4b77031c1
yunjuKimm <yunju.tech@gmail.com>
2025-01-16 10:01:46 +0900

    home controller

---
+++ src/main/java/com/ll/basic/domain/home/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.ll.basic.domain.home.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    @ResponseBody
+    public String home() {
+        return "Hello World!";
+    }
+
+}
