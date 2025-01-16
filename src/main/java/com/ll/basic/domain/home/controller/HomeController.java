ced38246c4fdb89463e7f5499a2ff372bf01ab68
yunjuKimm <yunju.tech@gmail.com>
2025-01-16 10:13:30 +0900

    introduce

--- src/main/java/com/ll/basic/domain/home/controller/HomeController.java
+++ src/main/java/com/ll/basic/domain/home/controller/HomeController.java
@@ -4,13 +4,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller
+@Controller // @Controller 어노테이션을 붙여주면 해당 클래스를 컨트롤러로 인식하게 된다.
 public class HomeController {
 
-    @GetMapping("/")
-    @ResponseBody
+    @GetMapping("/") // @GetMapping 어노테이션을 붙여주면 / 경로로 들어왔을 때 해당 메소드가 실행된다.
+    @ResponseBody // @ResponseBody 어노테이션을 붙여주면 해당 메소드의 리턴값을 브라우저에 보여준다.
     public String home() {
         return "Hello World!";
+    }
+
+    @GetMapping("/introduce")
+    @ResponseBody
+    public String introduce() {
+        return "안녕하세요. 저는 홍길동입니다.";
     }
 
 }
